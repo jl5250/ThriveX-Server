39872b68b58aed0c1912e680002c62f00389a435
jl5250 <1662528926@qq.com>
2025-04-24 14:01:48 +0800

    refactor: 重构file的代码！

--- blog/src/main/java/liuyuyang/net/web/controller/FileController.java
+++ blog/src/main/java/liuyuyang/net/web/controller/FileController.java
@@ -1,6 +1,5 @@
 package liuyuyang.net.web.controller;
 
-import cn.hutool.core.lang.Dict;
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import com.qiniu.common.QiniuException;
 import io.swagger.annotations.Api;
@@ -9,7 +8,6 @@ import liuyuyang.net.common.annotation.PremName;
 import liuyuyang.net.common.execption.CustomException;
 import liuyuyang.net.common.utils.Result;
 import liuyuyang.net.common.utils.OssUtils;
-import liuyuyang.net.web.service.impl.FileDetailService;
 import org.dromara.x.file.storage.core.FileInfo;
 import org.dromara.x.file.storage.core.FileStorageService;
 import org.dromara.x.file.storage.core.get.ListFilesResult;
@@ -20,8 +18,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.*;
 
@@ -39,30 +35,18 @@ public class FileController {
     @Resource
     private FileStorageService fileStorageService;
 
-    @Resource
-    private FileDetailService fileDetailService;
-
     @PremName("file:add")
     @PostMapping
     @ApiOperation("文件上传")
     @ApiOperationSupport(author = "刘宇阳 | liuyuyang1024@yeah.net", order = 1)
-    public Result<Object> add(@RequestParam(defaultValue = "") String dir,@RequestParam String platform, @RequestParam MultipartFile[] files) throws IOException {
+    public Result<Object> add(@RequestParam(defaultValue = "") String dir, @RequestParam MultipartFile[] files) throws IOException {
         if (dir == null || dir.trim().isEmpty()) throw new CustomException(400, "请指定一个目录");
 
         List<String> urls = new ArrayList<>();
 
-        String newPlatform=!Objects.equals(platform, "")? platform:OssUtils.getPlatform();
         for (MultipartFile file : files) {
-            BufferedImage image = ImageIO.read(file.getInputStream());
-            int width = image.getWidth(); // 通过图片流获取图片宽度
-            int height = image.getHeight(); // 通过图片流获取图片高度
-            // 创建一个Dict来存储长和宽
-            Dict dict = Dict.create();
-            dict.set("width", width);
-            dict.set("height", height);
             FileInfo result = fileStorageService.of(file)
-                    .setAttr(dict)
-                    .setPlatform(newPlatform)
+                    .setPlatform(OssUtils.getPlatform())
                     .setPath(dir + '/')
                     .upload();
 
@@ -129,68 +113,65 @@ public class FileController {
         return Result.success(list);
     }
 
+    @PremName("file:list")
     @GetMapping("/list")
     @ApiOperation("获取指定目录中的文件")
-    @ApiOperationSupport(author = "刘宇阳 | liuyuyang1024@yeah.net", order = 6)
-    public Result<List<Map>> getFileList(@RequestParam String dir,@RequestParam(defaultValue = "defaultPlatform") String platform) {
+    @ApiOperationSupport(author = "刘宇阳 | liuyuyang1024@yeah.net", order = 5)
+    public Result<Map<String, Object>> getFileList(
+            @RequestParam String dir,
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "20") Integer size
+    ) {
         if (dir == null || dir.trim().isEmpty()) throw new CustomException(400, "请指定一个目录");
-        String newPlatform=!Objects.equals(platform, "defaultPlatform")? platform:OssUtils.getPlatform();
+
         ListFilesResult result = fileStorageService.listFiles()
-                .setPlatform(newPlatform)
+                .setPlatform(OssUtils.getPlatform())
                 .setPath(dir + '/')
                 .listFiles();
 
         // 获取文件列表
-        List<Map> list = new ArrayList<>();
-        List<RemoteFileInfo> fileList = result.getFileList();
+        List<Map<String, Object>> fileList = new ArrayList<>();
+        List<RemoteFileInfo> remoteFileList = result.getFileList();
 
-        for (RemoteFileInfo item : fileList) {
+        // 按lastModified时间降序排序（最新的在前）
+        remoteFileList.sort((a, b) -> b.getLastModified().compareTo(a.getLastModified()));
+
+        // 计算分页参数
+        int total = remoteFileList.size();
+        int startIndex = (page - 1) * size;
+        int endIndex = Math.min(startIndex + size, total);
+
+        // 分页处理
+        List<RemoteFileInfo> pageList = remoteFileList.subList(startIndex, endIndex);
+
+        for (RemoteFileInfo item : pageList) {
             // 如果是目录就略过
             if (Objects.equals(item.getExt(), "")) continue;
 
             Map<String, Object> data = new HashMap<>();
-
             data.put("basePath", item.getBasePath());
             data.put("dir", dir);
             data.put("path", item.getBasePath() + item.getPath() + item.getFilename());
             data.put("name", item.getFilename());
             data.put("size", item.getSize());
             data.put("type", item.getExt());
+            data.put("date", item.getLastModified());
 
             String url = item.getUrl();
             if (!url.startsWith("https://")) url = "https://" + url;
-            Dict dict=fileDetailService.getByUrl(url).getAttr();
-            if(!dict.isEmpty())
-                data.put("arrt",fileDetailService.getByUrl(url).getAttr());
             data.put("url", url);
 
-            list.add(data);
+            fileList.add(data);
         }
 
-        return Result.success(list);
-    }
+        // 构建分页结果
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("result", fileList);
+        resultMap.put("size", size);
+        resultMap.put("page", page);
+        resultMap.put("pages", (total + size - 1) / size);
+        resultMap.put("total", total);
 
-    @GetMapping("/dir/local")
-    @ApiOperation("获取本地指定目录列表")
-    @ApiOperationSupport(author = "王俊龙 | 1662528926@qq.com", order = 7)
-    public Result<List<Map>> getLocalDirList(@RequestParam(defaultValue = "album/") String dir,@RequestParam(defaultValue = "local") String platform) {
-        // 使用传入的platform，如果没有传入则使用OssUtils.getPlatform()
-        ListFilesResult result = fileStorageService.listFiles()
-                .setPath(dir)
-                .setPlatform(platform)
-                .listFiles();
-
-        // 获取文件列表
-        List<Map> list = new ArrayList<>();
-        List<RemoteDirInfo> fileList = result.getDirList();
-
-        for (RemoteDirInfo item : fileList) {
-            Map<String, Object> data = new HashMap<>();
-            data.put("name", item.getName());
-            data.put("path", item.getOriginal());
-            list.add(data);
-        }
-
-        return Result.success(list);
+        return Result.success(resultMap);
     }
 }
